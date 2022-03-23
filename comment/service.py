02ef646d9147d2628e72f18a03d1d011e8d15032
golgol0 <heojungyeon.97@gmail.com>
2022-03-23 18:05:33 +0900

    Update service.py
    크롤링 코드 수정

--- comment/service.py
+++ comment/service.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import pandas as pd
 import time
-
 from selenium import webdriver
 
 class Service:
@@ -31,31 +30,36 @@ class Service:
                         url2 = baseurl + a_link
                         print('>> ' + url2)
 
-                        options = webdriver.ChromeOptions() 
-                        # options.add_argument('headless') 
-                        options.add_argument('disable-gpu') 
-                        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-                        options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4692.99 Safari/537.36, Referrer-Policy=no-referrer, strict-origin-when-cross-origin')
-                        driver = webdriver.Chrome('chromedriver', options=options)
-                        driver.get(url2)
-                        time.sleep(2)
-
-                        soup2 = BeautifulSoup(driver.page_source, 'lxml')
-                        comments = soup2.select('#focus_cmt > div.comment_wrap > div.comment_box > ul > li')
-                        
-                        for c in comments:
-                            try: 
-                                text = c.find('p', attrs={'class': 'usertxt'}).text
-                                if 'dc App' in text:
-                                    text = text.split('-')[0].strip('')
-                                print(text)
-                                comment_list.append(text) 
-                            except Exception as e:
-                                pass
-
-                        driver.quit()
+                        try: 
+                            options = webdriver.ChromeOptions() 
+                            # options.add_argument('headless') 
+                            options.add_argument('disable-gpu') 
+                            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+                            options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4692.99 Safari/537.36, Referrer-Policy=no-referrer, strict-origin-when-cross-origin')
+                            driver = webdriver.Chrome('chromedriver', options=options)
+                            driver.get(url2)
+                            time.sleep(5)
+                            soup2 = BeautifulSoup(driver.page_source, 'lxml')
+                            comments = soup2.select('#focus_cmt > div.comment_wrap > div.comment_box > ul > li')
+                            
+                            for c in comments:
+                                try: 
+                                    text = c.find('p', attrs={'class': 'usertxt'}).text
+                                    if 'dc App' in text:
+                                        text = text.split('-')[0].strip('')
+                                    print(text)
+                                    comment_list.append(text) 
+                                except Exception as e:
+                                    pass
+                                
+                        except Exception as e:
+                            print(e)
+                            pass
+                        finally:
+                            driver.quit()
 
             except Exception as e:
+                print(e)
                 pass
         
         data = pd.DataFrame(comment_list, columns=['댓글'])
